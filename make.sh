edaf8074d65a88913edf92358238c655f1bb9473
src3453 <info@example.com>
2024-03-02 22:15:20 +0900

    a

--- make.sh
+++ make.sh
@@ -5,6 +5,6 @@ VERSION_SUFFIX="a"
 VERSION_REVISION=$(git rev-list HEAD --count)
 VERSION_HASH=$(git log -1 --format=%H | cut -c 1-7)
 VERSION="v${VERSION_MAJOR}.${VERSION_REVISION}${VERSION_SUFFIX}.${VERSION_HASH}"
-rm ./pack.mcmeta
+rm ./pack.mcmetarrrrrr
 cat ./pack.mcmeta.in | sed "s/VERSION/${VERSION}/" >> ./pack.mcmeta
 zip "src3453s_KemomimiGirls_${VERSION}.zip" -r ./assets/** ./pack.png ./pack.mcmeta
