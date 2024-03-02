5e560587833231435a80a22f458b46e4820cb200
src3453 <info@example.com>
2024-03-02 15:18:24 +0900

    a

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+VERSION_MAJOR=0
+VERSION_SUFFIX="a"
+VERSION_REVISION=$(git rev-list HEAD --count)
+VERSION_HASH=$(git log -1 --format=%H | cut -c 1-7)
+VERSION="v${VERSION_MAJOR}.${VERSION_REVISION}${VERSION_SUFFIX}.${VERSION_HASH}"
+zip "src3453s_KemomimiGirls_${VERSION}.zip" -r ./assets/** ./pack.png ./pack.mcmeta
